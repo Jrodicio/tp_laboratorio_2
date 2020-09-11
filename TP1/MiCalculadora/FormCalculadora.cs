f6a943c919da3648df335c183ebf39e204d933a8
unknown <jrodicio98@gmail.com>
2020-09-11 00:12:20 -0300

    Se pushea nuevamente porlas

--- TP1/MiCalculadora/FormCalculadora.cs
+++ TP1/MiCalculadora/FormCalculadora.cs
@@ -30,7 +30,11 @@ namespace MiCalculadora
                 this.Close();
             } 
         }
-
+        /// <summary>
+        /// Limpiará los campos txt, cmb y lbl de FormCalculadora.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnLimpiar_Click(object sender, EventArgs e)
         {
             this.txtNumero1.Text = "";
@@ -38,7 +42,13 @@ namespace MiCalculadora
             this.cmbOperador.Text = "";
             this.lblResultado.Text = "0";
         }
-
+        /// <summary>
+        /// Realizará, en caso de ser posible, el cálculo entre numero1 y numero2.
+        /// </summary>
+        /// <param name="numero1">Primer número</param>
+        /// <param name="numero2">Segundo número</param>
+        /// <param name="operador">Operador</param>
+        /// <returns></returns>
         private static double Operar(string numero1, string numero2, string operador)
         {
             Numero num1 = new Numero(numero1);
@@ -46,7 +56,11 @@ namespace MiCalculadora
 
             return Calculadora.Operar(num1, num2, operador);
         }
-
+        /// <summary>
+        /// Realizá la operación seleccionada en cmbOperador, entre los valores de txtNumero1 y txtNumero2 de FormCalculadora.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnOperar_Click(object sender, EventArgs e)
         {
             string numero1 = this.txtNumero1.Text;
@@ -55,13 +69,21 @@ namespace MiCalculadora
 
             this.lblResultado.Text = Operar(numero1, numero2, operador).ToString();
         }
-
+        /// <summary>
+        /// Intentará convertir el valor de lblResultado a binario.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnConvertirABinario_Click(object sender, EventArgs e)
         {
             string resultado = this.lblResultado.Text;
             this.lblResultado.Text = Numero.DecimalBinario(resultado);
         }
-
+        /// <summary>
+        /// Intentará convertir el valor de lblResultado a decimal.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnConvertADecimal_Click(object sender, EventArgs e)
         {
             string resultado = this.lblResultado.Text;
